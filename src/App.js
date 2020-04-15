98158256207c8af7f359d2d9f4d0dc185bcfd9dd
Nenad Mucic <nenad.mucic@faxi.co.uk>
2020-04-15 18:01:53 +0200

    add router; add about page; add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigation from "./components/layout/Navigation";
 import Joke from "./components/Joke";
+import About from "./components/pages/About";
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <Joke />
+      <Router>
+        <Navigation />
+        <Route path="/" exact component={Joke} />
+        <Route path ="/about" exact component={About} />
+      </Router>
     </div>
   );
 }
